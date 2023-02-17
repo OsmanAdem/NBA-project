101af251fd2ad0d4b6b9d5a88775249dd57638b0
Osman Adem <112473033+OsmanAdem@users.noreply.github.com>
2023-02-17 01:52:36 +0100

    first commit

--- src/components/NBAGames.vue
+++ src/components/NBAGames.vue
@@ -1,11 +1,12 @@
 <template>
-    <!-- Props -->
     <div class="NBAGames">
+        <!-- Text Interpolation -->
         <h1>Welcome to the {{ league }}!</h1>
     </div>
 </template>
 
 <script>
+    /* props */
     export default {
         props: ['league']
     }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,13 +8,16 @@
     <div class="input">
         <h2 class="text">Write to see teams:</h2>
         <div>
+            <!-- v-model and v-on -->
             <input class="input-text" placeholder="Text" v-model="search" />
             <button class="btn" @click="fetchData">Press</button>
         </div>
         <ul>
-            <!-- v-for and v-if -->
+            <!-- v-for and v-bind --->
             <li v-for="team in teams" :key="team">
+                <!-- Text Interpolation -->
                 {{ team.full_name }}
+                <!-- v-if -->
                 <div v-if="team.full_name">NBA Team</div>
             </li>
         </ul>
